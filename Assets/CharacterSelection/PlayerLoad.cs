3b0188dcc44d335c0f59fcb1afbd9146f2bbce2f
gitAdrianK <14051526+gitAdrianK@users.noreply.github.com>
2023-03-17 19:59:33 +0100

    Housekeeping

--- Assets/CharacterSelection/PlayerLoad.cs
+++ Assets/CharacterSelection/PlayerLoad.cs
@@ -5,18 +5,21 @@ using TMPro;
 
 public class PlayerLoad : MonoBehaviour
 {
+    // The text fields presenting Pepe Maldini's Stats.
     [SerializeField] private TMP_Text pepeMaldiniName;
     [SerializeField] private TMP_Text pepeMaldiniLife;
     [SerializeField] private TMP_Text pepeMaldiniAttack;
     [SerializeField] private TMP_Text pepeMaldiniDefense;
     [SerializeField] private TMP_Text pepeMaldiniGold;
 
+    // The text fields presenting Zebra The Weebra's Stats.
     [SerializeField] private TMP_Text zebraTheWeebraName;
     [SerializeField] private TMP_Text zebraTheWeebraLife;
     [SerializeField] private TMP_Text zebraTheWeebraAttack;
     [SerializeField] private TMP_Text zebraTheWeebraDefense;
     [SerializeField] private TMP_Text zebraTheWeebraGold;
 
+    // The text fields presenting Custom's Stats.
     [SerializeField] private TMP_Text skillpoints;
     [SerializeField] private TMP_InputField customName;
     [SerializeField] private TMP_InputField customLife;
@@ -24,12 +27,14 @@ public class PlayerLoad : MonoBehaviour
     [SerializeField] private TMP_InputField customDefense;
     [SerializeField] private TMP_InputField customGold;
 
+    // Internal int version of customs stats to simplify skillpoint distribution and parsing.
     private int skillpointsInt;
     private int lifeInt;
     private int attackInt;
     private int defenseInt;
     private int goldInt;
 
+    // Start is called before the first frame update
     public void Start()
     {
         pepeMaldiniName.text = EntityPepeMaldini.playerName;
@@ -95,6 +100,10 @@ public class PlayerLoad : MonoBehaviour
         ChangeScene.LoadScene(ChangeScene.Scene.Game);
     }
 
+    /// <summary>
+    /// Modifies life.
+    /// </summary>
+    /// <param name="value">The value.</param>
     public void ModifyLife(int value)
     {
         if (!CanModify(value, skillpointsInt, lifeInt))
@@ -106,6 +115,10 @@ public class PlayerLoad : MonoBehaviour
         UpdateUI();
     }
 
+    /// <summary>
+    /// Modifies attack.
+    /// </summary>
+    /// <param name="value">The value.</param>
     public void ModifyAttack(int value)
     {
         if (!CanModify(value, skillpointsInt, attackInt))
@@ -117,6 +130,10 @@ public class PlayerLoad : MonoBehaviour
         UpdateUI();
     }
 
+    /// <summary>
+    /// Modifies defense.
+    /// </summary>
+    /// <param name="value">The value.</param>
     public void ModifyDefense(int value)
     {
         if (!CanModify(value, skillpointsInt, defenseInt))
@@ -128,6 +145,10 @@ public class PlayerLoad : MonoBehaviour
         UpdateUI();
     }
 
+    /// <summary>
+    /// Modifies gold.
+    /// </summary>
+    /// <param name="value">The value.</param>
     public void ModifyGold(int value)
     {
         if (!CanModify(value, skillpointsInt, goldInt))
@@ -139,6 +160,13 @@ public class PlayerLoad : MonoBehaviour
         UpdateUI();
     }
 
+    /// <summary>
+    /// Checks if the button pressed can modify the stats.
+    /// </summary>
+    /// <param name="value">The value.</param>
+    /// <param name="skillpoints">The skillpoints.</param>
+    /// <param name="skillvalue">The skillvalue.</param>
+    /// <returns> True if modifiable, false otherwise.</returns>
     private bool CanModify(int value, int skillpoints, int skillvalue)
     {
         if (value <= 0)
@@ -158,6 +186,10 @@ public class PlayerLoad : MonoBehaviour
         return true;
     }
 
+    /// <summary>
+    /// Updates skillpoints.
+    /// </summary>
+    /// <param name="value">The value.</param>
     private void UpdateSkillpoints(int value)
     {
         if (value <= 0)
@@ -170,13 +202,16 @@ public class PlayerLoad : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Updates UI.
+    /// </summary>
     private void UpdateUI()
     {
         skillpoints.text = skillpointsInt.ToString();
-        customLife.text = "Life " + lifeInt.ToString();
-        customAttack.text = "Attack " + attackInt.ToString();
-        customDefense.text = "Defense " + defenseInt.ToString();
-        customGold.text = "Gold " + goldInt.ToString();
+        customLife.text = "Life: " + lifeInt.ToString();
+        customAttack.text = "Attack: " + attackInt.ToString();
+        customDefense.text = "Defense: " + defenseInt.ToString();
+        customGold.text = "Gold: " + goldInt.ToString();
     }
 
     /// <summary>
